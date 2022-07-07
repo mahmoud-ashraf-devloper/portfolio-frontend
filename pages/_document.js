3c5847c5fc702f0eedec9b04a53e5e7bf9a86f62
Mahmoud Ashraf <88296664+mahmoud-ashraf-devloper@users.noreply.github.com>
2022-07-07 22:04:38 +0200

    done

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,11 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400&display=swap" rel="stylesheet" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com"  />
+                    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400&display=swap" rel="stylesheet" />
 
-</Head>
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
