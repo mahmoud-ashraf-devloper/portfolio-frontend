097ca2650936af444f3d7a0d6146b03b4b9b9881
Mahmoud Ashraf <88296664+mahmoud-ashraf-devloper@users.noreply.github.com>
2022-07-13 19:42:08 +0200

    done

--- components/About.jsx
+++ components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
           <p className='text-gray-600 my-2'>Hi, I am Mahmoud,</p>
           <div className='text-gray-400 text-md  leading-6'>
             <p>A freelancer Full-Stack Web developer with hands on experience with modern technologies and its updates latest updates. I have an experience in both backend and frontend.</p>
-            <h2 className='text-lg flex items-center'><RiBookmarkFill className='text-indigo-600 mr-1 w-5 h-4'  /> Technologies that i use</h2>
+            <h2 className='text-lg flex items-center'><RiBookmarkFill className='text-indigo-600 mr-1 w-5 h-4'  />Why Me?</h2>
             <ul className='ml-3'>
               <li className='flex items-center'><GiCheckMark className='text-indigo-600 mr-4 w-5 h-4' /> I am a responsable and reliable Person.</li>
               <li className='flex items-center'><GiCheckMark className='text-indigo-600 mr-4 w-5 h-4' /> I pay attenton to details.</li>
@@ -24,7 +24,7 @@ const About = () => {
               <li className='flex items-center'><GiCheckMark className='text-indigo-600 mr-4 w-5 h-4' /> Lastly cause i love this job and really cares and works really hard to get as high as i could in this career.</li>
             </ul>
 
-            <h1 className='text-lg flex items-center'><RiBookmarkFill className='text-indigo-600 mr-1 w-5 h-4' /> Technologies that i use</h1>
+            <h1 className='text-lg flex items-center'><RiBookmarkFill className='text-indigo-600 mr-1 w-5 h-4' />Technologies that i use</h1>
             <ul className='ml-4'>
               <li className='flex items-center'><GiCheckMark className='text-indigo-600 mr-4 w-5 h-4 ' /> for frontend i usually use react or nextjs depends on the project requirements and besiness demands. i build also pure projects without framworks with (Html, Css, Javascrip).</li>
               <br /><li className='flex items-center'><GiCheckMark className='text-indigo-600 mr-4 w-5 h-4 ' /> for the backend I have hands on experience with (PHP, Laravel framwork). i can develop apis with excelent documentaion.</li>
